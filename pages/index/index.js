66c9997dae3b534f52f527b7e51a12edd61922d0
hyq2015 <18030638805@163.com>
2017-01-11 17:07:46 +0800

    update

--- pages/index/index.js
+++ pages/index/index.js
@@ -1,61 +1,34 @@
-//index.js
-//获取应用实例
-var app = getApp()
+// pages/index/index.js
 Page({
-  data: {
-    tip:"",
-    username: '',
-    phone: ""
+  data:{
+    array: [
+      {
+        field1:"field1",
+        field2:"field2",
+        field3:"field3",
+        image: 'http://img06.tooopen.com/images/20160818/tooopen_sy_175833047715.jpg',
+      },
+      {
+        field1:"field1",
+        field2:"field2",
+        field3:"field3",
+        image: 'http://img06.tooopen.com/images/20160818/tooopen_sy_175833047715.jpg'
+      }
+    ]
   },
-  formBindsubmit:function(e){
-    if(e.detail.value.username.length==0||e.detail.value.phone.length==0){
-        this.setData({
-          tip:'提示：用户名和手机号不能为空！',
-          username:'',
-          phone:''
-        })
-    }else{
-        this.setData({
-          tip:'',
-          username:e.detail.value.username,
-          phone:e.detail.value.phone
-        });
-        console.log(this.data);
-        wx.request({
-          url: 'https://192.168.1.254/wxapp/rest/singUp',
-          data:e.detail.value,
-          method:"POST",
-          header: {
-              'content-type': 'application/json'
-          },
-          success: function(res) {
-            console.log(res.data)
-          },
-          fail: function(res) {
-            console.log(res.data)
-          },
-          complete: function(res) {
-            console.log(res.data)
-          }
-        });
-    }
+  onLoad:function(options){
+    // 页面初始化 options为页面跳转所带来的参数
   },
-  formReset:function(){
-    this.setData({
-      tip:'',
-      username:'',
-      phone:''
-    })
+  onReady:function(){
+    // 页面渲染完成
   },
-  onLoad: function () {
-    console.log('onLoad')
-    var that = this
-    //调用应用实例的方法获取全局数据
-    app.getUserInfo(function(userInfo){
-      //更新数据
-      that.setData({
-        userInfo:userInfo
-      })
-    })
+  onShow:function(){
+    // 页面显示
+  },
+  onHide:function(){
+    // 页面隐藏
+  },
+  onUnload:function(){
+    // 页面关闭
   }
 })
